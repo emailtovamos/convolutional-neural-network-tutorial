03ed0722c6f0fd596fe6838dd78a0692353b82d0
satyajit <emailtovamos@gmail.com>
2018-09-16 23:37:10 +0100

    Compilation & fitting added.

--- cnn1.py
+++ cnn1.py
@@ -22,6 +22,7 @@ classifier = Sequential()
 #Step 1 Convolution 
 classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
 
+
 #Step 2 Pooling 
 classifier.add(MaxPooling2D(pool_size= (2,2)))
 
@@ -29,5 +30,42 @@ classifier.add(MaxPooling2D(pool_size= (2,2)))
 classifier.add(Flatten())
 
 #Step 4 Full connection 
-classifier.add(Dense(output_dim = 128, activation = 'relu'))
-classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
+classifier.add(Dense(output_dim = 128,kernel_initializer ='uniform', activation = 'relu'))
+classifier.add(Dense(output_dim = 1,kernel_initializer ='uniform', activation = 'sigmoid')) 
+#Use softmax activation function if there were more than 2 outcomes
+
+#Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'] )
+#categorical cross entropy for many outputs
+
+
+# Part 2 - Fitting the CNN to the images
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+classifier.fit_generator(training_set,
+                         samples_per_epoch = 8000,
+                         nb_epoch = 25,
+                         validation_data = test_set,
+                         nb_val_samples = 2000)
+
+
+
+
